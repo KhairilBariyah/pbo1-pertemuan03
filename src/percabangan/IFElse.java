8dc8fea73e3de6dead629ae3f119e2f9ad063624
KhairilBariyah <yayahelzira@gmail.com>
2020-04-12 22:31:14 +0700

    IF ELSE conditonal  True statement1 benar

--- src/percabangan/IFElse.java
+++ src/percabangan/IFElse.java
@@ -5,10 +5,10 @@ public class IFElse {
         double totalBelanja = 53000;
         double uangDiDompet = 25000;
 
-        if(uangDiDompet<totalBelanja) {
-            System.out.println("Uang tidak mencukupi setelah Lockdown, Dirumahaja");
-        }else{
-            System.out.println("Uang mencukupi, anda selamat bisa belanja");
+        if (uangDiDompet < totalBelanja) {
+            System.out.println("Uang kurang, kurangi ya jajannya");
+        } else {
+            System.out.println("Uanng cukup, selamat deh anda");
         }
-    }
+    }    
 }
