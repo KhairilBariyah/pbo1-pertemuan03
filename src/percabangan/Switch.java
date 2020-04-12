fc08e83dd32244528ebe380497e66ecdb239207d
KhairilBariyah <yayahelzira@gmail.com>
2020-04-12 23:01:35 +0700

    switch jalan jalan

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("liat liatan seyum senyuman");
+                break;
+            case 2:
+                System.out.println("PDKT");
+                break;
+            case 3:
+                System.out.println("chatingan");
+                break;
+            case 4:
+                System.out.println("acayan");
+                break;
+            case 5:
+                System.out.println("dubil dit, jelan jelong,,Long Last...wkkkwkk");
+                break;
+        }
+    }
+}
