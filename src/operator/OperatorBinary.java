985b6162d8dfb7cb595e086df67f7d4cf77d8165
KhairilBariyah <yayahelzira@gmail.com>
2020-04-12 21:16:21 +0700

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasaan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+
+    String nama = "Khairil Bariyah";
+    int jumlahSks = 24;
+    double indeksPrestasikumulatif = 3.89;
+
+    
+    }
+}
